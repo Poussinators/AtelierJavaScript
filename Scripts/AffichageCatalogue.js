388586c13ca4a9f2e3ac18f563c7e3592b9e37d9
MattUnicornBlue <matthieu.noel@epsi.fr>
2020-11-26 00:34:38 +0100

    Traitement des descriptions trop longues

--- Scripts/AffichageCatalogue.js
+++ Scripts/AffichageCatalogue.js
@@ -8,11 +8,18 @@ function ajouterTagImageCatalogue(element) {
     let divInner = document.createElement("div");
     divInner.classList.add("articleBox");
     articleModel = document.getElementsByClassName('articleBox')[0]
+
+    // Couper les descriptions trop longues
+    var desc = element.description
+    if (desc.length >= 50) {
+        desc = desc.substring(0, 45) + ' ...'
+    }
+
     divInner.innerHTML = String(articleModel.innerHTML)
         .replaceAll('{{nom}}', element.name)
         .replaceAll('{{prix}}', element.price)
         .replaceAll('{{src}}', element.image)
-        .replaceAll('{{desc}}', element.description)
+        .replaceAll('{{desc}}', desc)
     let grandDiv = $("#articleInsert");
     grandDiv.append(divInner);
 }
