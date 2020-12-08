9bb5fd28c91cbaa14addd4cfac4100ea2d8a7a0c
Spiroti <56719843+Spiroti@users.noreply.github.com>
2020-12-08 12:07:31 +0100

    ajout des contraintes de la quantite d'article
    pb sur l'affichage

--- Scripts/GestionPanier.js
+++ Scripts/GestionPanier.js
@@ -7,24 +7,59 @@
     console.log(panier);
 
     function AjouterArticle(nom,image,quant,prix){
+        debugger;
         console.log(nom, image, quant, prix);
-        panier.push([nom,image,quant,prix]);
-        console.log(panier);
-        AfficherArticlePanier(nom,image,quant,prix);
+        ContrainteAjout(nom,quant,image,prix);
+        ViderPanier();
+        for (var index in panier) {
+            articlePanier = panier[index];
+            // s'il y a déja un objet
+            AfficherArticlePanier(articlePanier);
+        }
     }
     
-    function AfficherArticlePanier(nom,image,quant,prix) {
+    function ContrainteAjout(nom,quant,image,prix){
+        for (var index in panier) {
+            articlePanier = panier[index];
+            // s'il y a déja un objet
+            if(articlePanier.name == nom){
+                console.log("trouvé, article " + nom + " qut : " + quant);
+                articlePanier.quantite += quant;         
+                if(articlePanier.quantite >9){
+                    articlePanier.quantite =9;
+                }       
+                console.log(articlePanier.quantite);
+                return;
+            }
+        }
+        console.log(panier);
+        panier.push({"name":nom,"image":image,"quantite": quant,"prix":prix});
+        for (var index in panier) {
+            articlePanier = panier[index];
+            // s'il y a déja un objet
+            if(articlePanier.quantite >9){
+                articlePanier.quantite =9;
+            }
+        }
+        console.log(panier);
+    }
+
+    function AfficherArticlePanier(article) {
         var divInner = document.createElement("div");
         divInner.classList.add("articleBoxPanier");
         articlePanierModel = document.getElementsByClassName('articleBoxPanier')[0];
-        console.log()
-        var prixtotal = quant * prix;
+    
+        var prixtotal = article.quant * article.prix;
         console.log(prixtotal)
         divInner.innerHTML = String(articlePanierModel.innerHTML)
-            .replaceAll('{{nom}}', nom)
+            .replaceAll('{{nom}}', article.nom)
             .replaceAll('{{prixTotal}}', prixtotal)
-            .replaceAll('{{quant}}', quant)
-            .replaceAll('{{src}}', image)
+            .replaceAll('{{quant}}', article.quant)
+            .replaceAll('{{src}}', article.image)
         let grandDiv = $("#panierInsert");
         grandDiv.append(divInner);
+    }
+
+    function ViderPanier(){
+        $('#panierInsert').innerHTML = '';
     }
