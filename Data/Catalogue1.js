3fe55aa92353f6d7ce1b025a1b3abab8f3f0e216
MattUnicornBlue <matthieu.noel@epsi.fr>
2020-12-08 08:59:36 +0100

    Update Catalogue

--- Data/Catalogue1.js
+++ Data/Catalogue1.js
@@ -1,61 +1,61 @@
 var catalog = [{
         "name": "Bonnet",
         "description": "Un bonnet Licorne",
-        "image": "../Images/bonnet.jpg",
+        "image": "./Images/bonnet.jpg",
         "price": "29.95"
     },
     {
         "name": "Chaussettes",
         "description": "Une paire de chaussettes Licorne",
-        "image": "../Images/chaussettes.jpg",
+        "image": "./Images/chaussettes.jpg",
         "price": "14.95"
     },
     {
         "name": "Chaussure à talon",
         "description": "Un paire de chaussure à talon Licorne avec une description super longue qui fais vraiment ch*er non ?",
-        "image": "../Images/chaussure.jpg",
+        "image": "./Images/chaussure.jpg",
         "price": "39.99"
     },
     {
         "name": "Déguisement",
         "description": "Un déguisement Licorne",
-        "image": "../Images/deguisement.jpg",
+        "image": "./Images/deguisement.jpg",
         "price": "44.99"
     },
     {
         "name": "Gants",
         "description": "Une paire de gant Licorne",
-        "image": "../Images/gants.jpg",
+        "image": "./Images/gants.jpg",
         "price": "12.99"
     },
     {
         "name": "Lunettes",
         "description": "Des lunette Licorne",
-        "image": "../Images/lunettes.jpg",
+        "image": "./Images/lunettes.jpg",
         "price": "19.95"
     },
     {
         "name": "Pantalon",
         "description": "Un pantalon Licorne",
-        "image": "../Images/pantalon.jpg",
+        "image": "./Images/pantalon.jpg",
         "price": "49.99"
     },
     {
         "name": "Pull",
         "description": "Un pull Licorne",
-        "image": "../Images/pull.jpg",
+        "image": "./Images/pull.jpg",
         "price": "64.99"
     },
     {
         "name": "Slip",
         "description": "Un slip Licorne",
-        "image": "../Images/slip.jpg",
+        "image": "./Images/slip.jpg",
         "price": "12.99"
     },
     {
         "name": "T-Shirt",
         "description": "Un T-Shirt Licorne",
-        "image": "../Images/tshirt.jpg",
+        "image": "./Images/tshirt.jpg",
         "price": "19.99"
     }
 ]
