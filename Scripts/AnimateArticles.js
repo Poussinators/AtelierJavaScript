563e7a122b0309a6d54dafab0ebad018e88101b5
MattUnicornBlue <matthieu.noel@epsi.fr>
2021-01-07 16:55:23 +0100

    Mon code mes règles.

--- Scripts/AnimateArticles.js
+++ Scripts/AnimateArticles.js
@@ -2,32 +2,20 @@ console.log('L\'erreur qui dit "GET http://127.0.0.1:5501/%7B%7Bsrc%7D%7D 404 (N
 
 // Faire apparaitre les trucs pour mettre dans le panier.
 
-var elements = document.getElementsByClassName("articleBox");
+var elements = document.getElementsByClassName("articleBox")
 
 for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('mouseenter', (event) => {
-
-        // let articlePicture = event.target.getElementsByClassName('articlePicture')[0]
-
-        // let img = articlePicture.getElementsByTagName('IMG')[0]
-        // let box = articlePicture.getElementsByTagName('DIV')[0]
-
-        // img.style.width = '150px';
-        // $("img").animate({ width: "150px" });
-        // $("#box").animate({height: "100px"});
-        // reduce150px(img, parseInt(img.style.width.replace('px', '')))
-        // box.style.width = '150px';
-        // $("div").animate({ width: "150px" });
 
         let articlePicture = $(event.target).children('.articlePicture')
 
         let img = articlePicture.children('img')
         let box = articlePicture.children('div')
 
-        img.animate({ width: "150px" });
-        box.animate({ width: "150px" });
+        img.animate({ width: "150px" })
+        box.animate({ width: "150px" })
 
-    }, false);
+    }, false)
 }
 
 
@@ -35,25 +23,17 @@ for (var i = 0; i < elements.length; i++) {
 for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('mouseleave', (event) => {
 
-        // let articlePicture = event.target.getElementsByClassName('articlePicture')[0]
-
-        // let img = articlePicture.getElementsByTagName('IMG')[0]
-        // let box = articlePicture.getElementsByTagName('DIV')[0]
-
-        // img.style.width = '300px';
-        // box.style.width = '0px';
-
         let articlePicture = $(event.target).children('.articlePicture')
 
         let img = articlePicture.children('img')
         let box = articlePicture.children('div')
 
-        img.animate({ width: "300px" });
-        box.animate({ width: "0px" });
+        img.animate({ width: "300px" })
+        box.animate({ width: "0px" })
 
         document.activeElement.blur()
 
-    }, false);
+    }, false)
 }
 
 // Resize le titre de la banière en fonction de la taille de l'écran.
@@ -67,8 +47,8 @@ function resizeHeaderFontSize() {
 // Resize boutiqueContainer en fonction de la taille de la fenêtre
 
 function resizeboutiqueContainer() {
-    var maxWidth = document.getElementById('boutique').offsetWidth;
-    var width = '0px';
+    var maxWidth = document.getElementById('boutique').offsetWidth
+    var width = '0px'
 
     // console.log(maxWidth)
 
@@ -91,20 +71,20 @@ function resizeboutiqueContainer() {
 }
 
 function activateAjouterBouton(ev) {
-    quant = ev.target.value;
-    button = ev.target.parentElement.parentElement.children[2].children[0];
+    quant = ev.target.value
+    button = ev.target.parentElement.parentElement.children[2].children[0]
     if (quant > 0) {
-        button.classList.value = 'articleAjouterButton';
+        button.classList.value = 'articleAjouterButton'
     } else {
-        button.classList.value = 'articleAjouterButton articleAjouterButtonDisabled';
+        button.classList.value = 'articleAjouterButton articleAjouterButtonDisabled'
     }
 
 }
 
 window.addEventListener('resize', () => {
-    resizeHeaderFontSize();
-    resizeboutiqueContainer();
+    resizeHeaderFontSize()
+    resizeboutiqueContainer()
 })
 
-resizeHeaderFontSize();
-resizeboutiqueContainer();
+resizeHeaderFontSize()
+resizeboutiqueContainer()
