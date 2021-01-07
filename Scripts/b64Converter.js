9ed33730070438b497acb47b97e0f46a6bf03d55
MattUnicornBlue <matthieu.noel@epsi.fr>
2021-01-07 23:42:43 +0100

    The epic comit.

---
+++ Scripts/b64Converter.js
@@ -0,0 +1,13 @@
+function ConvertAllData_toB64(Data) {
+    oldContent = document.getElementById('main').innerHTML
+        // document.getElementById('main').style = 'background-image: url("./Style/Images/Epic_sax_guy.gif"); background-size: 50vw; background-repeat: no-repeat; background-position: top;'
+    document.getElementById('main').innerHTML = ''
+    document.getElementById('main').classList.value = 'epic'
+    new Audio('../Style/Images/Epic.notAnImage.mp3').play()
+
+    setTimeout(() => {
+        // document.getElementById('main').style = ''
+        document.getElementById('main').innerHTML = oldContent
+        document.getElementById('main').classList.value = ''
+    }, 6500)
+}
