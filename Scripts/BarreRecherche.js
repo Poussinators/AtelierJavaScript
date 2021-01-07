98d6d723a15bb3efa3d6ed451e03ecb0c0e63a1e
Thomas <44287699+Poussinators@users.noreply.github.com>
2021-01-07 22:11:48 +0100

    Barre de recherche

---
+++ Scripts/BarreRecherche.js
@@ -0,0 +1,18 @@
+function Rechercher() {
+    let input = document.getElementById('recherche').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('articleBox');
+
+    for (i = 1; i < x.length; i++) {
+
+        const article = x[i];
+        const nomArticle = article.getElementsByClassName('articleNom')[0].innerHTML;
+        if (!nomArticle.toLowerCase().includes(input)) {
+            article.style.display="none";
+        }
+        else {
+            article.style.display="";
+        }
+    }
+
+}
